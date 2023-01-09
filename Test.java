5ef85b08ba8c78ad8b812a81e643f918a2e5040b
HanPengYang1776 <hanpengyang001@gmail.com>
2023-01-09 13:09:39 +0800

    test

--- Test.java
+++ Test.java
@@ -1,19 +1,11 @@
+//老师写的最简单的程序，完成 1+1,体会
+
 public class Test {
-	
-	public static void main(String[] args) {
-		int a = 10;
-		int b = 20;
-		int c = 25;
-		int d = 25;
-		System.out.println("a + b = " + (a + b));
-		System.out.println("a - b = " + (a - b));
-		System.out.println("a * b = " + (a * b));
-		System.out.println("a / b = " + (a / b));
-		System.out.println("b % a = " + (b % a));
-		System.out.println("a++ = " + (a++));
-		System.out.println("a-- = " + (a--));
-		System.out.println("d++ = " + (d++));
-		System.out.println("++d = " + (++d));
-		
-	}
+
+               public static void main(String[] args)  {
+      
+                              int res = 1 + 1;
+                              //显示
+                              System.out.print("结果=" + res);   
+               }
 }
