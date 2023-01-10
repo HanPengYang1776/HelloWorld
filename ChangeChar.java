ee9db383de35061f89bfc97f2ead5eb3e237ad0c
HanPengYang1776 <hanpengyang001@gmail.com>
2023-01-10 11:11:13 +0800

    test

---
+++ ChangeChar.java
@@ -0,0 +1,17 @@
+//演示转义字符的使用
+public class ChangeChar {
+
+	//编写一个main方法
+	public static void main(String[] args) {
+
+//      \t  :一个制表位，实现对齐的功能
+		System.out.println("北京\t天津\t上海");
+// \n   :换行符
+// \\   :一个\
+// \"   :一个"
+// \'   :一个'
+// \r   :一个回车 System.out.println("韩顺平教育\r北京");
+
+	}
+
+}
