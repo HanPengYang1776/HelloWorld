7556983c2763e539d1796184204c70e1d5b0d31c
HanPengYang1776 <121745616+HanPengYang1776@users.noreply.github.com>
2023-01-22 23:49:31 +0800

    codecademy test11

--- Review.java
+++ Review.java
@@ -1,6 +1,6 @@
 public class Review {
 	public static void main(String[] args){
-		//The main method executes the tasks of the class
-		System.out.println("My first Java program from scratch!");
+	//The main method executes the tasks of the class
+    System.out.println("My first Java program from scratch!");
 	}
-}
+}    
